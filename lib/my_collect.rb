eca8d9f64557c40651eb2ec0dec599f9f9d1d8f9
Ninibeth Teixeira <ninibethreina@gmail.com>
2020-04-04 14:29:38 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
-
+def my_collect (array)
+  i=0 
+  capitalized_languages= []
+  while i< array.length 
+    capitalized_languages << yield(array[i])
+    i=i+1 
+    
+  end 
+  capitalized_languages
+end 
