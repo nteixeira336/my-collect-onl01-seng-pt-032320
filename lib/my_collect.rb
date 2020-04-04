c0c20ed6f4512eba789ed34931bda196909c6d0f
Ninibeth Teixeira <ninibethreina@gmail.com>
2020-04-04 14:26:08 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,7 +4,7 @@ def my_collect (array)
   while i< array.length 
     yield (array[i])
     i=i+1 
-    capitalized_languages << i 
+    capitalized_languages << array 
   end 
   capitalized_languages
 end 
